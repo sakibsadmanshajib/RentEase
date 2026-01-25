59f93c196ed59eb78a90517d4d0c948558483f2b
Sakib Sadman Shajib <mail@sakibsadmanshajib.com>
2026-01-25 11:41:33 +0600

    update versions

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,13 +6,15 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
 
-    const config = new DocumentBuilder()
-        .setTitle('API Gateway')
-        .setDescription('The API Gateway description')
-        .setVersion('0.0.1')
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api/docs', app, document);
+    if (process.env.ENABLE_SWAGGER === 'true') {
+        const config = new DocumentBuilder()
+            .setTitle('API Gateway')
+            .setDescription('The API Gateway description')
+            .setVersion('0.0.1')
+            .build();
+        const document = SwaggerModule.createDocument(app, config);
+        SwaggerModule.setup('api/docs', app, document);
+    }
 
     await app.listen(4000);
     console.log(`Application is running on: ${await app.getUrl()}`);
